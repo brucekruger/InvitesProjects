fda4d6c76c4ada3c09baf1781c283fe81824332f
Andrei Baranau <andrei_baranau1@epam.com>
2019-04-18 18:26:32 +0300

    Hello world app.

--- PartyInvites/Startup.cs
+++ PartyInvites/Startup.cs
@@ -16,6 +16,7 @@ namespace PartyInvites
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,7 +25,11 @@ namespace PartyInvites
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
             }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.UseRouting(routes =>
             {
