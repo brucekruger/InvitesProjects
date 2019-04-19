8a72ecb6a696ea42700d9e4153175b02d1c9ad1a
Andrei Baranau <andrei_baranau1@epam.com>
2019-04-19 13:17:36 +0300

    Creating and Applying the Database Migration.

--- PartyInvites/Startup.cs
+++ PartyInvites/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PartyInvites.Models;
 
 namespace PartyInvites
 {
@@ -16,6 +17,7 @@ namespace PartyInvites
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IRepository, EFRepository>();
             services.AddMvc();
         }
 
