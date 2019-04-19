d79f51c538b85d7fb3915315793a1428bad17af9
Andrei Baranau <andrei_baranau1@epam.com>
2019-04-19 13:48:01 +0300

    Small refactoring.

--- PartyInvites/Models/ApplicationDbContext.cs
+++ PartyInvites/Models/ApplicationDbContext.cs
@@ -1,13 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+
 namespace PartyInvites.Models
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext() { }
-        protected override void OnConfiguring(DbContextOptionsBuilder builder)
-        {
-            builder.UseSqlite("Filename=./PartyInvites.db");
-        }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) => builder.UseSqlite("Filename=./PartyInvites.db");
         public DbSet<GuestResponse> Invites { get; set; }
     }
 }
